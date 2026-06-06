2f88024c924c1eb1e46d4a94a60ed5386d27cb21
YongSeok <daverage1123@gmail.com>
2026-06-06 16:04:23 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/ncsmap/NcsmapApplication.java
@@ -0,0 +1,13 @@
+package com.ncsmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcsmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcsmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncsmap/NcsmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncsmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcsmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
